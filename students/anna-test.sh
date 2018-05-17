a415fd3034ce22ae88d5fa4ca9c2bf74a0aa834a
shteyn <tarna83@mail.ru>
2018-05-17 11:35:08 +0200

    some updates

--- students/anna-test.sh
+++ students/anna-test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "MYVAR is: $MYVAR"
-MYVAR="hi there"
-echo "MYVAR is: $MYVAR"
+
+echo "What is your name?"
+read USER_NAME
+echo "Hello $USER_NAME"
+echo "I will create you a file called ${USER_NAME}_file"
+touch "${USER_NAME}_file"
