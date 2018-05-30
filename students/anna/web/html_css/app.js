e0d78161089e819620bd488be87dc00a2f1c91c2
shteyn <tarna83@mail.ru>
2018-05-30 09:07:07 +0200

    Revert "some changes"
    This reverts commit c134e5601e196fbc9b276eef9f0feba91de81200.

--- students/anna/web/html_css/app.js
+++
@@ -1,7 +0,0 @@
-let a = 3
-function addTwo(x) {
-let ret = x + 2
-return ret
-}
-let b = addTwo(a)
-console.log(b)
--- students/anna/web/javascript/scripts/main.js
+++
@@ -1,4 +0,0 @@
-
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
